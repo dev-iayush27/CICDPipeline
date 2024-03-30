1ddf53e95b8355a650b3589570432ced84d7d313
Ayush Gupta <dev.iayush27@gmail.com>
2024-03-31 01:51:18 +0530

    Initial Commit

---
+++ CICDPipeline/CICDPipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDPipelineApp.swift
+//  CICDPipeline
+//
+//  Created by Ayush Gupta on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CICDPipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
